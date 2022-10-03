366c965fa4537bba956f98e169502301189e5299
Lucianosolari <luciano3924@gmail.com>
2022-10-03 18:22:53 -0300

    login

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
 import Table from "./components/Table/Table";
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom";
+import Login from "./components/Login/Login";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<NavBar/>}>
-      <Route path="/table" element={<Table/>}/>
+    <Route path="/" element={<NavBar />}>
+      <Route path="/table" element={<Table />} />
+      <Route path="/login" element={<Login />} />
     </Route>
   )
-)
-
-
+);
 
 function App() {
   return <RouterProvider router={router} />;
