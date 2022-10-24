0336dc6908bb9f56f3cdd99bd451181108272f7e
manuelibarbia <manuelibarbia02@gmail.com>
2022-10-24 20:15:17 -0300

    Inicio de de sesión

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,26 @@
 import { Link } from "react-router-dom";
+import { useRef, useState } from "react";
+import 'firebase/auth'
+import {UseFirebaseApp} from 'reactfire';
+
 import "./Login.css";
-import { useContext } from "react";
-import { authContext } from "../../context/AuthContext";
 
 const Login = () => {
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+
+  const loginEmailRef = useRef();
+  const loginPasswordRef = useRef();
+
+  const login = () => {
+    
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    login();
+  }
+
   return (
     <>
       <section className=" gradient-custom vh-100">
@@ -14,70 +31,74 @@ const Login = () => {
                 className="card bg-dark text-white" /*style="border-radius: 1rem;"*/
               >
                 <div className="card-body p-5 text-center">
-                  <div className="mb-md-5 mt-md-4 pb-5">
-                    <h2 className="fw-bold mb-2 text-uppercase">
-                      Iniciar sesión
-                    </h2>
-                    <p className="text-white-50 mb-5">
-                      Ingrese su mail y contraseña
-                    </p>
+                  <form onSubmit={handleSubmit}>
+                    <div className="mb-md-5 mt-md-4 pb-5">
+                      <h2 className="fw-bold mb-2 text-uppercase">
+                        Iniciar sesión
+                      </h2>
+                      <p className="text-white-50 mb-5">
+                        Ingrese su mail y contraseña
+                      </p>
 
-                    <div className="form-outline form-white mb-4">
-                      <input
-                        type="email"
-                        id="typeEmailX"
-                        className="form-control form-control-lg"
-                      />
-                      <label className="form-label" htmlFor="typeEmailX">
-                        Email
-                      </label>
+                      <div className="form-outline form-white mb-4">
+                        <input
+                          type="email"
+                          id="typeEmailX"
+                          className="form-control form-control-lg"
+                          ref={loginEmailRef}
+                        />
+                        <label className="form-label" htmlFor="typeEmailX">
+                          Email
+                        </label>
+                      </div>
+
+                      <div className="form-outline form-white mb-4">
+                        <input
+                          type="password"
+                          id="typePasswordX"
+                          className="form-control form-control-lg"
+                          ref={loginPasswordRef}
+                        />
+                        <label className="form-label" htmlFor="typePasswordX">
+                          Contraseña
+                        </label>
+                      </div>
+
+                      <p className="small mb-5 pb-lg-2">
+                        <a className="text-white-50" href="#!">
+                          ¿Olvidó su contraseña?
+                        </a>
+                      </p>
+
+                      <button
+                        className="btn btn-outline-light btn-lg px-5"
+                        type="submit"
+                      >
+                        Ingresar
+                      </button>
+
+                      <div className="d-flex justify-content-center text-center mt-4 pt-1">
+                        <a href="#!" className="text-white">
+                          <i className="fab fa-facebook-f fa-lg"></i>
+                        </a>
+                        <a href="#!" className="text-white">
+                          <i className="fab fa-twitter fa-lg mx-4 px-2"></i>
+                        </a>
+                        <a href="#!" className="text-white">
+                          <i className="fab fa-google fa-lg"></i>
+                        </a>
+                      </div>
                     </div>
 
-                    <div className="form-outline form-white mb-4">
-                      <input
-                        type="password"
-                        id="typePasswordX"
-                        className="form-control form-control-lg"
-                      />
-                      <label className="form-label" htmlFor="typePasswordX">
-                        Contraseña
-                      </label>
+                    <div>
+                      <p className="mb-0">
+                        ¿No tiene una cuenta?{" "}
+                        <Link className="text-white-50 fw-bold" to="newuser">
+                          Crear cuenta
+                        </Link>
+                      </p>
                     </div>
-
-                    <p className="small mb-5 pb-lg-2">
-                      <a className="text-white-50" href="#!">
-                        ¿Olvidó su contraseña?
-                      </a>
-                    </p>
-
-                    <button
-                      className="btn btn-outline-light btn-lg px-5"
-                      type="submit"
-                    >
-                      Ingresar
-                    </button>
-
-                    <div className="d-flex justify-content-center text-center mt-4 pt-1">
-                      <a href="#!" className="text-white">
-                        <i className="fab fa-facebook-f fa-lg"></i>
-                      </a>
-                      <a href="#!" className="text-white">
-                        <i className="fab fa-twitter fa-lg mx-4 px-2"></i>
-                      </a>
-                      <a href="#!" className="text-white">
-                        <i className="fab fa-google fa-lg"></i>
-                      </a>
-                    </div>
-                  </div>
-
-                  <div>
-                    <p className="mb-0">
-                      ¿No tiene una cuenta?{" "}
-                      <Link className="text-white-50 fw-bold" to="newuser">
-                        Crear cuenta
-                      </Link>
-                    </p>
-                  </div>
+                </form>
                 </div>
               </div>
             </div>
