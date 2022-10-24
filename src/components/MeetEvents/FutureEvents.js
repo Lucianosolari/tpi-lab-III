06c9cab917eb19741a143a617c4a18506c2fd4a8
manuelibarbia <manuelibarbia02@gmail.com>
2022-10-24 19:20:43 -0300

    Metiendo estilo y empleando validación

--- src/components/MeetEvents/FutureEvents.js
+++ src/components/MeetEvents/FutureEvents.js
@@ -4,16 +4,13 @@ import EventsCard from "../EventsCard/EventsCard";
 
 const FutureEvents = ({ date, title, organizer, location }) => {
   return (
-    <>
       <EventsCard>
         <p>{date}</p>
         <h2>{title}</h2>
         <p>{organizer}</p>
         <p>{location}</p>
-
         <button type="button">Inscribirme</button>
       </EventsCard>
-    </>
   );
 };
 
