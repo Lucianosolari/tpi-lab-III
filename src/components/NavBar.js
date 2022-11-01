e255f5e20e1cde69b5c57281e57cd5ce55fd7cb6
Lucianosolari <luciano3924@gmail.com>
2022-10-31 21:45:42 -0300

    Rol user - admin

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,3 +1,4 @@
+import { useContext } from "react";
 import { Button } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
@@ -6,23 +7,30 @@ import { useAuth } from "../context/AuthContext";
 import "./NavBar.css";
 
 const NavBar = () => {
-  const {user, logout, loading} = useAuth();
+  const { user, logout, loading, role } = useAuth();
   const navigate = useNavigate();
   const logoutHandler = async () => {
     await logout();
-    navigate('/login');
-  }
+    navigate("/login");
+  };
   const loginHandler = async () => {
-    navigate('/login');
-  }
+    navigate("/login");
+  };
 
-  if (loading) return <h1>Cargando página principal...</h1>
+  const adminHandler = async () => {
+    navigate("/add-event");
+  };
+
+  if (loading) return <h1>Cargando página principal...</h1>;
   return (
     <>
       <Navbar bg="light" expand="lg">
         <Container fluid>
           <NavLink to="/events">Futuros eventos</NavLink>
           <NavLink to="/table">Resultados</NavLink>
+          {role === "admin" && (
+            <Button onClick={adminHandler}>Agregar evento</Button>
+          )}
           {!user && <Button onClick={loginHandler}>Iniciar sesión</Button>}
           {user && <Button onClick={logoutHandler}>Cerrar sesión</Button>}
         </Container>
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,5 +1,10 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 import { auth } from "../firebase";
 import { getAllUsers } from "../lib/api";
 
@@ -13,6 +18,7 @@ export const useAuth = () => {
 export function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [role, setRole] = useState("");
 
   const signUp = (email, password) =>
     createUserWithEmailAndPassword(auth, email, password);
@@ -22,29 +28,37 @@ export function AuthProvider({ children }) {
 
   const logout = () => signOut(auth);
 
-const compareUser = async (user) => {
-  const userData = await getAllUsers();
-  console.log(user)
-  userData.forEach(u => {
-    if (u.email === user.email) {
-      let loggedUser = u
-      return loggedUser
-    }
-  });
-  console.log(userData);
-}
-const loggedUser = compareUser(user)
-console.log(loggedUser.role)
+  const compareUser = async (user) => {
+    const userData = await getAllUsers();
 
-useEffect(() => {
-  onAuthStateChanged(auth, currentUser => {
-    setUser(currentUser);
-    compareUser(currentUser);
-    setLoading(false);
-  })
-}, []);
+    userData.forEach((u) => {
+      if (u.email === user.email) {
+        let loggedUser = u;
+
+        setRole(loggedUser.role);
+      }
+    });
+  };
+
+  console.log(role);
+
+  const dispararComparacion = (user) => {
+    compareUser(user);
+  };
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+      dispararComparacion(currentUser);
+    });
+  }, []);
 
   return (
-    <authContext.Provider value={{ signUp, login, logout, loading, user }}>{children}</authContext.Provider>
+    <authContext.Provider
+      value={{ signUp, login, logout, loading, user, role }}
+    >
+      {children}
+    </authContext.Provider>
   );
 }
